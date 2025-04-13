31f2c412c9c1237e8278cd0d0ee1bdfeb989b702
Sebastian Strus <sebastianstrus@Sebastians-MacBook-Air.local>
2025-04-13 20:04:11 +0200

    Hide empty categories

--- Polyglot Pro/View/Vocabulary/VocabularyViewModel.swift
+++ Polyglot Pro/View/Vocabulary/VocabularyViewModel.swift
@@ -14,21 +14,15 @@ class VocabularyViewModel: ObservableObject {
     @Published private(set) var categoriesBySection: [Category.CatSection: [Category]] = [:]
     
     init() {
-        // Load initial data
         refreshCategories()
     }
     
     func refreshCategories() {
-        // Built-in categories grouped by section
         var combinedCategories = Dictionary(grouping: Category.builtInCases, by: { $0.catSection })
         
-        // Load and add custom categories to the .custom section
         let customCategories = CustomCategoryManager.shared.loadCustomCategories()
         combinedCategories[.custom] = customCategories
         
-        // Update the published property
-        categoriesBySection = combinedCategories
-        
-        print("Loaded categories: \(categoriesBySection)")
+        categoriesBySection = combinedCategories.filter { !$0.value.isEmpty }
     }
 }
